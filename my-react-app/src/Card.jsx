eea5f23614d6b51b3bf9e978c84b455b11d049bf
zakir1999 <m.zakirhossen99@gmail.com>
2025-02-27 18:01:20 +0600

    React Props

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,14 @@
+import reactpic from './assets/react.png'
+function Card(){
+    return (
+       
+        <div className="card">
+            <img className="card-img" src={reactpic}></img>
+            <h1 className="title">React-JS</h1>
+            <p className="detials">This is really help  for web Application. Without it you can't survive.</p>
+
+        </div>
+    
+    );
+}
+export default Card
